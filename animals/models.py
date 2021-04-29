64878b7d12c6da15593336c01ae9160410687f5f
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-29 17:25:50 -0300

    feat: adds animals models relationships

--- animals/models.py
+++ animals/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    scientific_name = models.CharField(max_length=255)
+
+
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     age = models.FloatField()
     weight = models.FloatField()
     sex = models.CharField(max_length=255)
 
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
 
-class Group(models.Model):
-    name = models.CharField(max_length=255)
-    scientific_name = models.CharField(max_length=255)
+
+class Characteristic(models.Model):
+    characteristic = models.CharField(max_length=255)
+    animals = models.ManyToManyField(Animal)
